d0bf45139497cace59da247a0a71fdbb8334049e
IoanRowlands <ioan.rowlands07@gmail.com>
2024-10-30 17:19:17 +0000

    Display squad list and graph of the selected team from the DataFrame

---
+++ 01.LeagueOne-2024-2025/LeagueOneTable.py
@@ -0,0 +1,38 @@
+# libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def DisplayResults(t):
+    print(t)
+    titlestring = (t.Squad + " Results")
+    dataValues = np.array([t.W, t.D, t.L])
+    mylabels = ["Win", "Draw", "Loss"]
+    dataColours = ["Green", "Yellow", "Red"]
+
+    plt.pie(dataValues, labels = mylabels, colors=dataColours)
+    plt.legend(title = titlestring)
+    plt.show() 
+
+
+# fbref League One 2024-2025 League Table link
+url_df = 'https://fbref.com/en/comps/15/League-One-Stats'
+
+# Read and get league table
+df = pd.read_html(url_df)[0]
+print (df)
+
+# loop through the rows using iterrows()
+
+# Get team at the top of the table
+#team = df.iloc[0]
+#DisplayResults(team)
+
+# Get Wrexham from dataframe
+
+df.set_index('squad')
+wrexham = df.loc[df['Squad'] == 'Wrexham']
+
+print(wrexham)
+
+#DisplayResults(wrexham)
---
+++ source/01.LeagueOneTable/LeagueOneTable.py
@@ -0,0 +1,25 @@
+# libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# fbref League One 2024-2025 League Table link
+url_df = 'https://fbref.com/en/comps/15/League-One-Stats'
+
+# Read and get league table
+df = pd.read_html(url_df)[0]
+print (df)
+
+# Get list of teams from league table 
+print("This is a list of data associated with League One: ") 
+print(df.columns)
+
+print(df.index[0])
+
+# loop through the rows using iterrows()
+team = df.iloc[1]
+
+labels = 'Wins', 'Draws', 'Losses'
+sizes = [team.W , team.D , team.L]
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels)
